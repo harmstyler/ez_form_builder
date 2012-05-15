037696802f9ee533e66a5dbbf3a85f1dca4cc570
Tyler Harms <tyler.harms@gmail.com>
2012-05-14 20:53:55 -0500

    Update to include form builder in the tabs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-ez_form_builder
-===============
+eZ Publish Form Builder
+=======================
 
-eZ Publish extension for building forms
+eZ Publish extension for building forms. Based off of cformsII for Wordpress.
---
+++ settings/menu.ini.append.php
@@ -0,0 +1,26 @@
+<?php /* #?ini charset="utf-8"?
+
+[NavigationPart]
+Part[eztagsnavigationpart]=eZFormBuilder
+
+[TopAdminMenu]
+Tabs[]=ezformbuilder
+
+[Topmenu_ezformbuilder]
+NavigationPartIdentifier=ezformbuildersnavigationpart
+Name=eZ Form Builder
+Tooltip=eZ Form Builder dashboard
+URL[]
+URL[default]=forms/dashboard
+Enabled[]
+Enabled[default]=true
+Enabled[browse]=false
+Enabled[edit]=false
+Shown[]
+Shown[default]=true
+Shown[edit]=true
+Shown[navigation]=true
+Shown[browse]=true
+
+PolicyList[]=forms/read
+*/ ?>
